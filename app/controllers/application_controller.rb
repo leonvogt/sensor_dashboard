54b9baed208ecb4894f7b382684df4356ad8e2b1
Leon <nonick@nonick.ch>
2023-02-18 20:44:06 +0100

    Add translation button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_locale
+
+  def set_locale
+    if current_user.present? && ['de', 'en'].include?(params[:locale])
+      current_user.update_attribute(:locale, params[:locale])
+    end
+    I18n.locale = params[:locale] || current_user&.locale || I18n.default_locale
+  end
 end
