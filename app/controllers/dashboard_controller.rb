13a233ce4fe934dd5012b2f44fb8bec552a2f3fb
Leon <nonick@nonick.ch>
2023-02-20 22:11:13 +0100

    Display AVGs of sensor_data instead of showing all

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
   def show
+    @sensors = current_user.sensors.includes(:sensor_data, :device)
   end
 end
--- app/services/serialize_sensor_data.rb
+++ app/services/serialize_sensor_data.rb
@@ -4,15 +4,29 @@ class SerializeSensorData
     @sensor_data = sensor_data
   end
 
-  def serialize
+  def serialize(with_avg: :avg_of_each_hour)
+    case with_avg
+    when :avg_of_each_hour
+      sensor_data = avg_of_each_hour
+    end
     {
       chart_type:   @sensor.chart_type,
       value_suffix: SensorConfiguration::SENSOR_TYPE_SUFFIX[@sensor.sensor_type],
-      min_value:    @sensor_data.map(&:value).min,
-      max_value:    @sensor_data.map(&:value).max,
-      avg_value:    @sensor_data.map(&:value).sum || 1 / @sensor_data.count.round(2) || 1,
-      timestamps:   @sensor_data.map(&:created_at),
-      values:       @sensor_data.map(&:value)
+      min_value:    sensor_data.map { |data| data[:value] }.min,
+      max_value:    sensor_data.map { |data| data[:value] }.max,
+      avg_value:    sensor_data.map { |data| data[:value] }.sum || 1 / @sensor_data.count.round(2) || 1,
+      timestamps:   sensor_data.map { |data| data[:timestamp] },
+      values:       sensor_data.map { |data| data[:value] }
     }
+  end
+
+  def avg_of_each_hour
+    sensor_data = @sensor_data.group_by { |data| data.created_at.beginning_of_hour }
+    sensor_data = sensor_data.map do |key, value|
+      {
+        timestamp: key,
+        value:     (value.map(&:value).sum / value.count).round(2)
+      }
+    end
   end
 end
