f5af239b75296a1b2cf3891ed1af1b939499aabb
Leon <nonick@nonick.ch>
2023-03-07 22:19:24 +0100

    Add development-queue to Sidekiq's queue list

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,4 +71,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.hosts = nil
+
+  # Use a real queuing backend for Active Job (and separate queues per environment).
+  config.active_job.queue_name_prefix = "sensor_dashboard_development"
 end
