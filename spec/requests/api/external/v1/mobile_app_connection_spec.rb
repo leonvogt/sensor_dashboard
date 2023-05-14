26f8307ca6168e2928a224b1f24a4c91f8c3cf26
Leon <nonick@nonick.ch>
2023-05-14 19:25:32 +0200

    Remove `unique_mobile_id` from test. Adapted Test to send `authentication_token` instead of logging in with devise.

--- spec/requests/api/external/v1/mobile_app_connection_spec.rb
+++ spec/requests/api/external/v1/mobile_app_connection_spec.rb
@@ -1,25 +1,24 @@
 require 'rails_helper'
 
 RSpec.shared_context 'MobileApp requests' do
-  def send_request(unique_mobile_id: SecureRandom.hex, notification_token: SecureRandom.hex, platform: 'android', app_version: '1.0.0')
-    post api_v1_mobile_app_connections_url, params: {
-      mobile_app_connection: {
-        unique_mobile_id: unique_mobile_id,
+  def send_request(notification_token: SecureRandom.hex, platform: 'android', app_version: '1.0.0', authentication_token: '121212121212')
+    post(
+      api_v1_mobile_app_connections_url,
+      params: {
         notification_token: notification_token,
         platform: platform,
         app_version: app_version
+      },
+      headers: {
+        'Authorization': "Bearer #{authentication_token}"
       }
-    }
+    )
     expect(response).to be_successful
   end
 end
 
 RSpec.describe 'MobileAppConnection', type: :request do
-  let!(:current_user)   { create :user }
-
-  before do
-    login_as(current_user)
-  end
+  let!(:current_user)   { create :user, authentication_token: '121212121212' }
 
   describe '#create' do
     include_context 'MobileApp requests'
@@ -29,17 +28,10 @@ RSpec.describe 'MobileAppConnection', type: :request do
     end
 
     it 'Renews only the app_version' do
-      existing_connection = create :mobile_app_connection, unique_mobile_id: '123', notification_token: '123', app_version: '1.0.0'
+      existing_connection = create :mobile_app_connection, notification_token: '123', app_version: '1.0.0'
       expect {
-        send_request(unique_mobile_id: existing_connection.unique_mobile_id, notification_token: '123', app_version: '2.0.0')
+        send_request(notification_token: '123', app_version: '2.0.0')
       }.to change { existing_connection.reload.app_version }
-    end
-
-    it 'Renews a NotificationToken' do
-      existing_connection = create :mobile_app_connection, unique_mobile_id: '123', notification_token: '123'
-      expect {
-        send_request(unique_mobile_id: existing_connection.unique_mobile_id, notification_token: '456')
-      }.to change { existing_connection.reload.notification_token }
     end
   end
 end
