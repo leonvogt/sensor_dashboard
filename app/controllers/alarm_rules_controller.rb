77f24cee0d4e013abae8230642b085725f359035
Leon <nonick@nonick.ch>
2023-05-17 10:23:54 +0200

    Improve AlarmRule deletion process

--- app/controllers/alarm_rules_controller.rb
+++ app/controllers/alarm_rules_controller.rb
@@ -4,10 +4,6 @@ class AlarmRulesController < ApplicationController
     @alarm_rule = @sensor.alarm_rules.new
   end
 
-  def destroy
-    binding.pry
-  end
-
   private
   def set_sensor
     @sensor = Sensor.find(params[:sensor_id])
--- app/models/alarm_rule.rb
+++ app/models/alarm_rule.rb
@@ -2,11 +2,19 @@ class AlarmRule < ApplicationRecord
   RULE_TYPES = %w(min_value max_value)
 
   belongs_to :sensor
-  has_many :rule_violations
+  has_many :rule_violations, dependent: :destroy
 
   validates :rule_type, :value, presence: true
   validates :value, numericality: true
 
   scope :max_values, -> { where(rule_type: 'max_value') }
   scope :min_values, -> { where(rule_type: 'min_value') }
+
+  before_destroy :delete_rule_violations, prepend: true
+
+  private
+  # Delete rule violations manually, so we only execute one query instead of one per rule violation.
+  def delete_rule_violations
+    rule_violations.delete_all
+  end
 end
